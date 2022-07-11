152a35482c4cc8aebe07c4675efbcfdb90e8ce04
sehyeon1104 <sehyeon1104@naver.com>
2022-07-11 20:07:55 +0900

    게임잼 첫날 UI 구축

---
+++ Assets/01.Script/Gizoo/SwipeRes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwipeRes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image rect;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rect.rectTransform.sizeDelta = new Vector2(Screen.width, 1080);
+        Debug.Log(Screen.width);
+    }
+}
