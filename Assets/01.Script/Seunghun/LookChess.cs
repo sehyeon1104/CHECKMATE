8c7228c07f5a1705709f9a18458f6c5025638e75
Ggm-gijoo <gijoo1012@gmail.com>
2022-07-12 15:16:26 +0900

    스폰 시스템 조정

--- Assets/01.Script/Seunghun/LookChess.cs
+++ Assets/01.Script/Seunghun/LookChess.cs
@@ -5,8 +5,27 @@ using DG.Tweening;
 
 public class LookChess : MonoBehaviour
 {
-    private void OnEnable()
+    private Transform playerTransform;
+    private float blockRadius = 1f;
+
+    private void Start()
     {
-        transform.DOMove(new Vector2(GameManager.Instance.Player.position.x, GameManager.Instance.Player.position.y), 1f).SetEase(Ease.OutQuart);
+        playerTransform = GameObject.FindWithTag("Player").transform;
+        Move(playerTransform, blockRadius);
     }
+
+    private void Update()
+    {
+        if(transform.position == playerTransform.position)
+        {
+            Destroy(gameObject, Sync_Gijoo.Instance.tikTime);
+        }
+    }
+
+    public void Move(Transform playerTransform, float tileRad)
+    {
+        transform.DOMove(transform.position + new Vector3((playerTransform.position.x - transform.position.x) > 0 ? 4 : (playerTransform.position.x - transform.position.x) < 0 ? -4 : 0 * tileRad, (playerTransform.position.y - transform.position.y) > 0 ? 4 : (playerTransform.position.y - transform.position.y) < 0 ? -4 : 0 * tileRad),Sync_Gijoo.Instance.tikTime);
+    }
+
+
 }
--- Assets/01.Script/Seunghun/Testing.cs
+++ Assets/01.Script/Seunghun/Testing.cs
@@ -6,8 +6,9 @@ using System.IO;
 public class Wave
 {
     public string type;//몬스터 종류
-    public int x;
-    public int y;
+    //public int x;
+    //public int y;
+    public string keyboardArrow;
     public bool multi;
     public int childCount;
     // 스폰딜레이
@@ -15,38 +16,37 @@ public class Wave
 
 public class Testing : MonoBehaviour
 {
-    private enum ChessMal {  Pawn, Knight, Bishop, Rook, King, Queen};
+    private enum ChessMal { Pawn, Knight, Bishop, Rook, King, Queen };
 
-    ChessMal chessState = ChessMal.Pawn; 
+    ChessMal chessState = ChessMal.Pawn;
 
     private void Awake()
     {
 
         spawnList = new List<Wave>();
-        ReadSpawnFile("N1");
-        //int ran = Random.Range(0, 3);
-        //switch (ran)
-        //{
-        //    case 0:
-        //        ReadSpawnFile("patternA");
-        //        break;
-        //    case 1:
-        //        ReadSpawnFile("patternB");
-        //        break;
-        //    case 2:
-        //        ReadSpawnFile("patternC");
-        //        break;
-        //}
+        int ran = Random.Range(0, 3);
+        switch (ran)
+        {
+            case 0:
+                ReadSpawnFile("N1");
+                break;
+            case 1:
+                ReadSpawnFile("N2");
+                break;
+            case 2:
+                ReadSpawnFile("N3");
+                break;
+        }
 
     }
     public List<GameObject> monsterMob = new List<GameObject>();
-    
+
 
     private Grid grid;
     public float cellSize;
 
     private int[,] arr;
-   
+
     private int[,] gridArray;
     void Start()
     {
@@ -55,7 +55,7 @@ public class Testing : MonoBehaviour
 
         gridArray = new int[5, 5];
 
-        
+
 
         //적들을 담는 걸 소환 
         //텍스트로 만들어놓음
@@ -64,45 +64,44 @@ public class Testing : MonoBehaviour
 
     }
 
- 
+
 
     public List<Wave> spawnList;
     public int spawnIndex; //다음녀석 다음녀석
     public bool spawnEnd;
 
-    public int bpm;
     double currentTime = 0d;
 
 
     bool isMultiSPawn = true;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         currentTime += Time.deltaTime;
-        
-        if(currentTime >=  (60d / bpm) && !spawnEnd && isMultiSPawn == true && isRead == true)
+
+        if (currentTime >= (60f / Sync_Gijoo.Instance.musicBpm) && !spawnEnd && isMultiSPawn == true && isRead == true)
         {
-            MonsterSpawn(); 
-            currentTime -=  60d / bpm;
+            MonsterSpawn();
+            currentTime -= 60f / Sync_Gijoo.Instance.musicBpm;
             //25 - 5; 
 
         }
-        else if(spawnEnd == true && isRead == true)
+        else if (spawnEnd == true && isRead == true)
         {
-            //int ran = Random.Range(0, 3);
-            //switch(ran)
-            //{
-            //    case 0:
-            //        ReadSpawnFile("patternA");
-            //        break;
-            //    case 1:
-            //        ReadSpawnFile("patternB");
-            //        break;
-            //    case 2:
-            //        ReadSpawnFile("patternC");
-            //        break;
-            //}
-           
+            int ran = Random.Range(0, 3);
+            switch (ran)
+            {
+                case 0:
+                    ReadSpawnFile("N1");
+                    break;
+                case 1:
+                    ReadSpawnFile("N2");
+                    break;
+                case 2:
+                    ReadSpawnFile("N3");
+                    break;
+            }
+
         }
     }
 
@@ -119,22 +118,22 @@ public class Testing : MonoBehaviour
         TextAsset textFile = Resources.Load(patern) as TextAsset;//텍스트 파일 에셋 클래스
         StringReader stringReader = new StringReader(textFile.text); //파일 내의 문자열 읽기 클래스
 
-
-        while(stringReader != null)
+        while (stringReader != null)
         {
             string line = stringReader.ReadLine(); //한줄씩 반환
 
 
-            if(line == null)
+            if (line == null)
             {
-                break; 
+                break;
             }
             Wave spawnData = new Wave();
             spawnData.type = line.Split(',')[0];
-            spawnData.x = int.Parse(line.Split(',')[1]);
-            spawnData.y = int.Parse(line.Split(',')[2]);
-            spawnData.multi = bool.Parse(line.Split(',')[3]);
-            spawnData.childCount = int.Parse(line.Split(',')[4]);
+            //spawnData.x = int.Parse(line.Split(',')[1]);
+            //spawnData.y = int.Parse(line.Split(',')[2]);
+            spawnData.keyboardArrow = line.Split(',')[1];
+            spawnData.multi = bool.Parse(line.Split(',')[2]);
+            spawnData.childCount = int.Parse(line.Split(',')[3]);
             spawnList.Add(spawnData); //변수를 초기화하고 변수를 넣은걸 추가한다.
         }
 
@@ -169,10 +168,10 @@ public class Testing : MonoBehaviour
             {
                 //3번반복이 안되네
                 //3개가 되나
-               
+
                 //자식중에 멀티하는게 있다면은 
 
-                
+
                 Debug.Log(spawnList[spawnIndex].childCount);
                 int enemyIndex = 0;
 
@@ -205,7 +204,7 @@ public class Testing : MonoBehaviour
                         break;
                 }
 
-                if(enemyIndex == 5)
+                if (enemyIndex == 5)
                 {
                     spawnIndex++;
                     return;
@@ -213,40 +212,74 @@ public class Testing : MonoBehaviour
 
                 GameObject enemy = monsterMob[enemyIndex];
 
-                int enemyPointX = spawnList[spawnIndex].x; 
-                int enemyPointY = spawnList[spawnIndex].y;
+                int X = 0;
+                int Y = 0;
 
-                Debug.Log(  "EnemyPointX"  + enemyPointX);
-                Debug.Log("EnemyPointY" + enemyPointY);
+                //Debug.Log(  "EnemyPointX"  + enemyPointX);
+                //Debug.Log("EnemyPointY" + enemyPointY);
+
+                switch (spawnList[spawnIndex].keyboardArrow)
+                {
+                    case "W":
+                        X = 2;
+                        Y = 4;
+                        break;
+                    case "S":
+                        X = 2;
+                        Y = 0;
+                        break;
+                    case "A":
+                        X = 0;
+                        Y = 2;
+                        break;
+                    case "D":
+                        X = 4;
+                        Y = 2;
+                        break;
+                    case "AW":
+                    case "WA":
+                        X = 4;
+                        Y = 0;
+                        break;
+                    case "DW":
+                    case "WD":
+                        X = 4;
+                        Y = 4;
+                        break;
+                    case "SA":
+                    case "AS":
+                        X = 0;
+                        Y = 0;
+                        break;
+                    case "SD":
+                    case "DS":
+                        X = 4;
+                        Y = 0;
+                        break;
+                }
 
                 //텍스트로 적소환차기
-                Vector2 monsterSpawnPostion = grid.GetWorldPosition(enemyPointX, enemyPointY);
-                Vector2 monsterPostionSet = new Vector2(monsterSpawnPostion.x + 0.5f, monsterSpawnPostion.y + 0.5f);
+                Vector2 monsterSpawnPostion;
+                Vector2 monsterPostionSet;
+                if (chessState == ChessMal.Bishop || chessState == ChessMal.Rook)
+                {
+                    monsterSpawnPostion = grid.GetWorldPosition(X, Y);
+                    monsterPostionSet = new Vector2(monsterSpawnPostion.x*2 + 0.5f, monsterSpawnPostion.y*2 + 0.5f);
+                }
+                else
+                {
+                    monsterSpawnPostion = grid.GetWorldPosition(X, Y);
+                    monsterPostionSet = new Vector2(monsterSpawnPostion.x + 0.5f, monsterSpawnPostion.y + 0.5f);
+                }
+               
 
-                Debug.Log(grid.GetWorldPosition(enemyPointX, enemyPointY));
+                Debug.Log(grid.GetWorldPosition(X, Y));
                 //Prefab를 가져온다. 
                 //Enum으로 가져오는 오브젝트를 정하는거야
                 Instantiate(enemy, monsterPostionSet, Quaternion.identity);
 
 
                 spawnIndex++;
-
-                //isMulti = false;
-                //isMulti = spawnList[spawnIndex].multi;
-                //if (isMulti == true)
-                //{
-                //    //i와 카운트를 다시 조정
-                //    i = 0;
-                //    count = spawnList[spawnIndex].childCount;
-                //    //그럼 다시 for문을 도나?
-                //}
-
-
-
-            
-
-             
-
 
 
 
@@ -258,10 +291,10 @@ public class Testing : MonoBehaviour
                 }
 
             }
-           isMultiSPawn = true;
+            isMultiSPawn = true;
 
         }
-        else if(isMulti == false)
+        else if (isMulti == false)
         {
             int enemyIndex = 0;
 
@@ -301,18 +334,66 @@ public class Testing : MonoBehaviour
             }
 
 
+
+
             GameObject enemy = monsterMob[enemyIndex];
-
-            int enemyPointX = spawnList[spawnIndex].x;
-            int enemyPointY = spawnList[spawnIndex].y;
-
+            int X = 0;
+            int Y = 0;
 
 
-            //텍스트로 적소환차기
-            Debug.Log(enemyPointX);
-            Debug.Log(enemyPointY);
-            Vector2 monsterSpawnPostion = grid.GetWorldPosition(enemyPointX, enemyPointY);
-            Vector2 monsterPostionSet = new Vector2(monsterSpawnPostion.x + 0.5f, monsterSpawnPostion.y + 0.5f);
+            switch (spawnList[spawnIndex].keyboardArrow)
+            {
+                case "W":
+                    X = 2;
+                    Y = 4;
+                    break;
+                case "S":
+                    X = 2;
+                    Y = 0;
+                    break;
+                case "A":
+                    X = 0;
+                    Y = 2;
+                    break;
+                case "D":
+                    X = 4;
+                    Y = 2;
+                    break;
+                case "AW":
+                case "WA":
+                    X = 4;
+                    Y = 0;
+                    break;
+                case "DW":
+                case "WD":
+                    X = 4;
+                    Y = 4;
+                    break;
+                case "SA":
+                case "AS":
+                    X = 0;
+                    Y = 0;
+                    break;
+                case "SD":
+                case "DS":
+                    X = 4;
+                    Y = 0;
+                    break;
+            }
+
+
+            Vector2 monsterSpawnPostion;
+            Vector2 monsterPostionSet;
+            if (chessState == ChessMal.Bishop || chessState == ChessMal.Rook)
+            {
+                monsterSpawnPostion = grid.GetWorldPosition(X, Y);
+                monsterPostionSet = new Vector2(monsterSpawnPostion.x * 2 + 1f, monsterSpawnPostion.y * 2 + 1f);
+            }
+            else
+            {
+                monsterSpawnPostion = grid.GetWorldPosition(X, Y);
+                monsterPostionSet = new Vector2(monsterSpawnPostion.x + 0.5f, monsterSpawnPostion.y + 0.5f);
+            }
             //Prefab를 가져온다. 
             //Enum으로 가져오는 오브젝트를 정하는거야
             Instantiate(enemy, monsterPostionSet, Quaternion.identity);
@@ -331,5 +412,5 @@ public class Testing : MonoBehaviour
 
 
     }
-  
+
 }
