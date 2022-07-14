f5fbca41fd81b977cdbb583bcd6a8a128b320f17
sehyeon1104 <sehyeon1104@naver.com>
2022-07-14 11:21:03 +0900

    병합

--- Assets/01.Script/Seunghun/CountDownControllder.cs
+++ Assets/01.Script/Seunghun/CountDownControllder.cs
@@ -5,6 +5,7 @@ using TMPro;
 using UnityEngine.UI;
 using DG.Tweening;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 using UnityEngine.Rendering.Universal;
 public class CountDownControllder : MonoSingleton<CountDownControllder>
 {
@@ -12,36 +13,60 @@ public class CountDownControllder : MonoSingleton<CountDownControllder>
     VolumeProfile profile;
     Vignette vig;    
     public int countDownTime;
-
+    MotionBlur volum;
     public TextMeshProUGUI countDownDisPlay;
     // public Sprite[] image;
     public Color[] color;
     public Image backGroundImage;
     //public Image numberImage;
+
+    public Camera uiCamera;
+    public MotionBlur motionBlur;
+
     //public Sprite[] sprites;
     private void Awake()
     {
         profile.TryGet(out vig);
+
+
+        profile.TryGet(out motionBlur);
     }
+
+    bool isCount = false;
+
+
+
+
     private void Update()
     {
-        switch(countDownTime)
+        if (isCount == false) return;
+
+        switch (countDownTime)
         {
             case 3:
-                vig.intensity.Override(0.3f);
+                vig.intensity.Override(0.233f);
                 break;
             case 2:
-                vig.intensity.Override(0.6f);
+                vig.intensity.Override(0.467f);
                 break;
             case 1:
-                vig.intensity.Override(0.8f);
+                vig.intensity.Override(0.7f);
                 break;
+                
         }
+
     }
     public void TextStart()
     {
         if (Instance != null)
         {
+            isCount = true;
+
+            var cameraData = uiCamera.GetUniversalAdditionalCameraData();
+            cameraData.renderPostProcessing = true;
+
+            cameraData.renderType = CameraRenderType.Overlay;
+            motionBlur.intensity.value = 0f;
             StartCoroutine(CountDownToStart());
         }
         
@@ -52,9 +77,9 @@ public class CountDownControllder : MonoSingleton<CountDownControllder>
     {
        
         while(countDownTime > 0)
-        {   
+        {
 
-            
+       
             countDownDisPlay.text = countDownTime.ToString();
             //backGroundImage.sprite = image[0];
             backGroundImage.color = color[spriteColor];
@@ -69,7 +94,9 @@ public class CountDownControllder : MonoSingleton<CountDownControllder>
         //텍스트 애니메이션 이 끝나면
         yield return new WaitForSeconds(Sync_Gijoo.Instance.tikTime);
 
-        Loader.Load(Loader.Scene.Seunghun);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
 
         countDownDisPlay.gameObject.SetActive(false);
     }
--- Assets/ArrowBlock.cs
+++ Assets/ArrowBlock.cs
@@ -14,8 +14,12 @@ public class ArrowBlock : MonoBehaviour
     public Testing testing;
 
     bool isActive = false;
-
+    public GameObject spriteK;
+    public GameObject spriteArrow;
     public GameObject breakKing;
+
+    //public UnityEngine.Rendering.Universal.Light2D king;
+
     //인터페이스로 그걸 만들까 
     private IEnumerator OnTriggerStay2D(Collider2D collision)
     {
@@ -45,9 +49,9 @@ public class ArrowBlock : MonoBehaviour
 
                    Sync_Gijoo.Instance.IsDeadTik();
                     //텍스트를 띄우는 함수
-                   
-                    //화면 가까이 하는 코드
 
+                    //화면 가까이 하는 코드
+                    spriteArrow.SetActive(false);
                     yield return StartCoroutine(CameraZoooooooooom.Instance.CameraZoom());
 
 
@@ -55,10 +59,13 @@ public class ArrowBlock : MonoBehaviour
                     //그리고 기주야 LookChess오류 나가지고 새로운 게임오브젝트만들고 Player태그달아 
                     testing.isSpawn = false; //소환하지 말게
                     //플레이어가 움지깅ㅁ
-                    transform.parent.gameObject.transform.DOShakePosition(0.75f, 0.3f, 24, 1f, false, true).OnComplete(()=>
+                    transform.parent.gameObject.transform.DOShakePosition(0.4f, 0.2f, 24, 1f, false, true).OnComplete(()=>
                     {
                         //함수 호출해가지고 
-                        transform.parent.gameObject.SetActive(false); //플레이어 펄스(삭제와 같은)
+                        //transform.parent.gameObject.SetActive(false); //플레이어 펄스(삭제와 같은)
+
+                        
+                        spriteK.SetActive(false);
 
                         GameObject obj = Instantiate(breakKing, transform.position, Quaternion.identity);
      
